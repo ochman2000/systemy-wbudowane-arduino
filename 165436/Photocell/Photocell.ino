91ab075ea7fbfb3548dbd0470e2ee36fb6858db1
Piotr Kluch <piterozaur@gmail.com>
2014-11-21 11:32:16 +0100

    Function descriptions updated

--- 165436/Photocell/Photocell.ino
+++ 165436/Photocell/Photocell.ino
@@ -16,6 +16,13 @@
  }
  
  // Returns the analog value from input connected to the LDR
+// Arudino contains 10-bit, 8 channels digital converter
+// It will map voltages between 0 and 5 into integer values
+// between 0 and 1023. This yields the resolution between
+// redings of: 5 volts / 1024 units or 0.0049 volts (4.9mV) per unit.
+// The input range can be changed using analogReference().
+// It takes about 100 microseconds (0.0001s) to read an analog input.
+// The maximum reading rate is about 10,000 times a second.
  int readPhotocellValue(int LDR) {
    return analogRead(LDR);
  }
