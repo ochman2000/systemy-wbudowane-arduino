9b588a58cec02a2c7ba93f65ecf33450ffb5d44e
Lukasz Ochmanski <ochman2000@gmail.com>
2014-12-20 04:42:42 +0100

    Zadanie gotowe do oddania

--- 183566/_07_timer/_07_timer.ino
+++ 183566/_07_timer/_07_timer.ino
@@ -1,11 +1,11 @@
 /*
  * Ten program odlicza 10 sekund i wyświetla tylko te cyfry, które nie są zerem.
  *
- * 2014-11-09, Warszawa, Polska
+ * 2014-12-20, Warszawa, Polska
  * Systemy Wbudowane Politechnika Łódzka
  * Łukasz Ochmański 183566
  */
-
+#include <TimerOne.h>
 #include <Wire.h>
 #include <TWIDisplay.h>
 
@@ -25,54 +25,54 @@ long minimalnyCzasWcisniecia = 50;
 volatile int i;
 
 void setup() {
-  pinMode(buttonPin, INPUT);
-  pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, enabled);
-  
-  Timer1.initialize(100000); // set a timer of length 100000 microseconds (or 0.1 sec - or 10Hz => the led will blink 5 times, 5 cycles of on-and-off, per second)
-  Timer1.attachInterrupt( timerIsr ); // attach the service routine here
-  
-  Wire.begin();
-  Serial.begin(9600);
-  Serial.println("Serial Display Test");
-  
-  disp.clear();
-  disp.setBrightness(255);
+    pinMode(buttonPin, INPUT);
+    pinMode(ledPin, OUTPUT);
+    digitalWrite(ledPin, enabled);
+
+    // set a timer of length 10000 microseconds (or 0.01 sec - or 100Hz)
+    Timer1.initialize(10000);
+    Timer1.attachInterrupt( timerIsr ); // attach the service routine here
+
+    Wire.begin();
+    Serial.begin(9600);
+    Serial.println("Serial Display Test");
+
+    disp.clear();
+    disp.setBrightness(255);
 }
 
 void loop()
-{  
-  disp.setBrightness(255);
-  if (i == -100) { i=999; }
-  if (i>0) {
-    int odczyt = digitalRead(buttonPin);
-  if (odczyt != ostatni) {
-    czasWcisniecia = millis();
-  }
- 
- if (minalMinimalnyOdstepCzasowy()) {
-    if (nastapilaZmiana(odczyt) && jestPrzycisniety()) {
-        zatrzymajZegar();
-    }
-  }
-  ostatni = odczyt; 
-//    if (enabled) i--;
-    writeCyfra(i);
-    disp.setDots(0,1,0,0);
-    delay(10);
-  } else {
-  
-  disp.writeStr("SIUP");
+{
+    disp.setBrightness(255);
+    if (i < -100) { i=1000; }
+    if (i>0) {
+        int odczyt = digitalRead(buttonPin);
+        if (odczyt != ostatni) {
+            czasWcisniecia = millis();
+        }
 
-  for (int j = 1000; j>0; j--) {
-    int a = j%50;
-    if (a>0 && a<25)
-      disp.setBrightness(255);
-    else
-      disp.setBrightness(0);
-    delay(2);
-  }    
-  }
+        if (minalMinimalnyOdstepCzasowy()) {
+            if (nastapilaZmiana(odczyt) && jestPrzycisniety()) {
+                zatrzymajZegar();
+            }
+        }
+        ostatni = odczyt;
+        writeCyfra(i);
+        disp.setDots(0,1,0,0);
+        delay(10);
+    } else {
+
+        disp.writeStr("SIUP");
+
+        for (int j = 1000; j>0; j--) {
+            int a = j%50;
+            if (a>0 && a<25)
+                disp.setBrightness(255);
+            else
+                disp.setBrightness(0);
+            delay(2);
+        }
+    }
 }
 
 /// --------------------------
@@ -80,53 +80,52 @@ void loop()
 /// --------------------------
 void timerIsr()
 {
-    // Toggle LED
-//    digitalWrite( 13, digitalRead( 13 ) ^ 1 );
     if (enabled) i--;
 }
 
 void writeCyfra(int val)
 {
-  char m_data[4];
-  
-  m_data[3] = val % 10;
-  val /= 10;
-  m_data[2] = val % 10;
-  val /= 10;
-  m_data[1] = val % 10;
-  val /= 10;
-  m_data[0] = val % 10;
-  
-  disp.clear();
-  Wire.beginTransmission(SLAVE_ADDR);
-  for (int i = 0; i <= 3; i++) {
-    //zauważyłem, że powinienem tutaj sprawdzać czy zero jest z lewej strony
-    //wstawianie pustego znaku nie powinno następować z prawej strony. 
-    if (m_data[i]==0 && (i==0 || (i!=0 && m_data[i-1]==0))) {
-      Wire.write(' ');
+    char m_data[4];
+
+    m_data[3] = val % 10;
+    val /= 10;
+    m_data[2] = val % 10;
+    val /= 10;
+    m_data[1] = val % 10;
+    val /= 10;
+    m_data[0] = val % 10;
+
+    disp.clear();
+    Wire.beginTransmission(SLAVE_ADDR);
+    for (int i = 0; i <= 3; i++) {
+        //zauważyłem, że powinienem tutaj sprawdzać czy zero jest z lewej strony
+        //wstawianie pustego znaku nie powinno następować z prawej strony.
+        if (m_data[i]==0 && (i==0 || (i!=0 && m_data[i-1]==0))) {
+            Wire.write(' ');
+        }
+        else {
+            Wire.write(m_data[i]);
+        }
     }
-    else {
-      Wire.write(m_data[i]);
-    }
-  }
-  Wire.endTransmission();
+    Wire.endTransmission();
 }
 
 boolean jestPrzycisniety() {
-  return obecny == HIGH;
+    return obecny == HIGH;
 }
 
 boolean nastapilaZmiana(int odczyt) {
-  boolean b = odczyt != obecny;
-  if (b) { obecny = !obecny; }
-  return b;
+    boolean b = odczyt != obecny;
+    if (b) {        obecny = !obecny;    }
+    return b;
 }
 
 boolean minalMinimalnyOdstepCzasowy() {
-  return (millis() - czasWcisniecia) > minimalnyCzasWcisniecia;
+    return (millis() - czasWcisniecia) > minimalnyCzasWcisniecia;
 }
 
 void zatrzymajZegar() {
-  enabled = !enabled;
-  digitalWrite(ledPin, enabled);
+    enabled = !enabled;
+    digitalWrite(ledPin, enabled);
+
 }
